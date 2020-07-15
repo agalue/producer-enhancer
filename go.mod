0a769217e44eba88806febac517e316d8a9f6950
Alejandro Galue <agalue@opennms.org>
2020-07-15 13:21:03 -0400

    Adding tests to verify enhancers behavior

--- go.mod
+++ go.mod
@@ -9,4 +9,5 @@ require (
 	github.com/lovoo/goka v1.0.0
 	github.com/pkg/errors v0.9.1 // indirect
 	google.golang.org/protobuf v1.25.0
+	gotest.tools v2.2.0+incompatible
 )
---
+++ model/alarm_test.go
@@ -0,0 +1,51 @@
+package model
+
+import (
+	"context"
+	"testing"
+
+	"github.com/agalue/producer-enhancer/protobuf/producer"
+	"github.com/lovoo/goka"
+	"github.com/lovoo/goka/tester"
+	"gotest.tools/assert"
+)
+
+func TestAlarmProcessor(t *testing.T) {
+	// Build and run processor
+	tt := tester.New(t)
+	group := new(EnhancedAlarm).DefineGroup("test", "alarms", "nodes", "enhanced")
+	proc, err := goka.NewProcessor(nil, group, goka.WithTester(tt))
+	assert.NilError(t, err)
+	go proc.Run(context.Background())
+	// Create tracker for the enhanced topic
+	mt := tt.NewQueueTracker("enhanced")
+	// Create node
+	node := &producer.Node{
+		Id:       1,
+		Label:    "test01",
+		Location: "Default",
+	}
+	tt.SetTableValue(goka.Table("nodes"), "1", node)
+	// Create alarm
+	alarm := &producer.Alarm{
+		Id: 11,
+		NodeCriteria: &producer.NodeCriteria{
+			Id: 1,
+		},
+		Uei:          "uei.opennms.org/test",
+		ReductionKey: "uei.opennms.org/test::1",
+		Description:  "This is a test",
+		LogMessage:   "This is a test",
+	}
+	tt.Consume("alarms", "uei.opennms.org/test::1", alarm)
+	// Verify enhanced alarm
+	if _, value, ok := mt.Next(); ok {
+		enhanced := value.(*EnhancedAlarm)
+		assert.Assert(t, enhanced.Alarm != nil)
+		assert.Equal(t, "This is a test", enhanced.Alarm.LogMessage)
+		assert.Assert(t, enhanced.Node != nil)
+		assert.Equal(t, "test01", enhanced.Node.Label)
+	} else {
+		t.Fail()
+	}
+}
