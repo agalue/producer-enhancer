a39f76da736b77ca31b93eb27467df05ad722f9e
Alejandro Galue <agalue@opennms.org>
2021-03-28 09:59:57 -0400

    Fix protobuf generation.

--- protobuf/build.sh
+++ protobuf/build.sh
@@ -2,4 +2,4 @@
 
 type protoc >/dev/null 2>&1 || { echo >&2 "protoc required but it's not installed; aborting."; exit 1; }
 
-protoc -I . opennms-kafka-producer.proto --go_out=./producer
+protoc --proto_path=./ --go_out=./ opennms-kafka-producer.proto
